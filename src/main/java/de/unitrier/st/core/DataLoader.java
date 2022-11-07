f550c135032728cde97c9474d45b0684a7acbaff
s4pideam <s4pideam@uni-trier.de>
2022-11-07 19:01:22 +0100

    init

---
+++ src/main/java/de/unitrier/st/core/DataLoader.java
@@ -0,0 +1,31 @@
+package de.unitrier.st.core;
+
+import java.io.*;
+
+public class DataLoader {
+
+    public String loadJsonFile(String name) {
+        //File file = new File(getClass().getResource("/"+name).getFile());
+		File file = new File("src/main/resources/"+name);
+        StringBuilder json = new StringBuilder();
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String str;
+            while ((str = br.readLine()) != null) {
+                System.out.println(str);
+                json.append(str);
+            }
+            br.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(json.length());
+
+        return json.toString();
+    }
+
+}
---
+++ src/test/java/DataTest.java
@@ -0,0 +1,18 @@
+import de.unitrier.st.core.CountryDataPoint;
+import de.unitrier.st.core.DataLoader;
+import de.unitrier.st.core.DataParser;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+
+public class DataTest {
+
+    @Test
+    public void tmpTest() {
+        Assert.assertEquals(true, true);
+    }
+
+}
