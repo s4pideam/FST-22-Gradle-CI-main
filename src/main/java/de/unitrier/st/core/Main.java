8854075a3a290a1e01db3f5724e42cf998d742d6
s4pideam <s4pideam@uni-trier.de>
2022-11-07 22:14:07 +0100

    Dublicate something for SonarTest
    dataParser = new DataParser();

--- src/main/java/de/unitrier/st/core/Main.java
+++ src/main/java/de/unitrier/st/core/Main.java
@@ -7,6 +7,9 @@ public class Main {
     public static void main(String[] args) {
         DataLoader dataLoader = new DataLoader();
         DataParser dataParser = new DataParser();
+        dataParser = new DataParser();
+        dataParser = new DataParser();
+        dataParser = new DataParser();
         String json = dataLoader.loadJsonFile("countries-aggregated_json.json");
         dataParser.parseJsonData(json);
     }
